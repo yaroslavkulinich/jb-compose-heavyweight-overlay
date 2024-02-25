e26bbc5e94fa42a495ed20d8c73cd8e03fa7fdf7
kulya <yaros.kulinich@gmail.com>
2024-02-25 13:41:12 +0100

    WIP;

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,34 +3,42 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0-beta5"//"1.0.0-beta6-dev450"
+    kotlin("jvm") version "1.9.22"
+    id("org.jetbrains.compose") version "1.5.12"
 }
 
 group = "ua.kulya.jbcomposeheavyweightoverlay"
 version = "1.0"
 
 repositories {
-    jcenter()
     google()
     mavenCentral()
+    maven ("https://jogamp.org/deployment/maven")
+    maven("https://jitpack.io")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    implementation(fileTree("libs") { include("*.jar") })
-    implementation("com.metsci.ext.org.jogamp.jogl:jogl-all-main:2.4.0-rc-20200202")
-    implementation("com.metsci.ext.org.jogamp.gluegen:gluegen-rt-main:2.4.0-rc-20200202")
+    implementation("com.github.imptah:worldwindjava:stable-SNAPSHOT")
+//    implementation("org.jogamp.gluegen:gluegen-rt-main:2.4.0")
+//    implementation("org.jogamp.jogl:jogl-all-main:2.4.0")
+//    implementation("org.codehaus.jackson:jackson-core-asl:1.9.13")
     implementation(compose.desktop.currentOs)
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "18"
 }
 
 compose.desktop {
     application {
         mainClass = "MainKt"
+        jvmArgs += listOf(
+            "--add-exports=java.base/java.lang=ALL-UNNAMED",
+            "--add-exports=java.desktop/sun.awt=ALL-UNNAMED",
+            "--add-exports=java.desktop/sun.java2d=ALL-UNNAMED",
+            "-XX:+AllowRedefinitionToAddDeleteMethods"
+        )
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "jb-compose-heavyweight-overlay"
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -24,16 +23,18 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import gov.nasa.worldwind.BasicModel
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas
-import kotlinx.coroutines.delay
+import java.awt.Dimension
+import java.util.*
 import javax.swing.BoxLayout
 import javax.swing.JPanel
 
 // Purpose of this sample is to show the trick how to draw and control(click responses) Compose elements over Heavyweight Swing/AWT components like Maps, VideoPlayers etc...
 fun main() = application {
+    setupSkikoRenderAPI()
     Window(
         onCloseRequest = ::exitApplication,
         title = "Compose for Desktop",
-        state = rememberWindowState(width = 800.dp, height = 500.dp)
+        state = rememberWindowState(width = 1000.dp, height = 600.dp)
     ) {
         MaterialTheme {
             RootUI()
@@ -43,71 +44,120 @@ fun main() = application {
 
 @Composable
 fun RootUI() {
-    SwingPanel( // <--- Swing panel as root for hierarchy where drawing over heavyweight components needed (Swing/Compose switching trick START)
-        modifier = Modifier.fillMaxSize().background(Color.Yellow),
-        factory = {
-            ComposePanel().apply { // <--- Switch back to Compose world for layout management purposes (Swing/Compose switching trick END)
-                setContent {
-                    val selectedMode = remember { mutableStateOf(Mode.Control) }
-                    Box { // <--- Here we use Box to make layering of SwingPanels
-
-                        // Bottom MAP layer
-                        SwingPanel( // <--- Here we use Swing panel to show WorldWindGLCanvas from Swing world (proper use of SwingPanel)
-                            modifier = Modifier.fillMaxSize().background(Color.Black),
-                            factory = { createJPanelWithWorldWindMap() }
-                        )
-
-                        // Map OVERLAY layer
-                        SwingPanel( // <--- Here we use Swing panel to make a trick wrapping Compose layout (Swing/Compose switching trick START). If we do not wrap Compose with SwingPanel here, you will not see Compose content over WorldWindGLCanvas(Swing)
-                            modifier = Modifier.width(60.dp).fillMaxHeight().align(Alignment.CenterStart),
-                            factory = {
-                                ComposePanel().apply { // <-- Swing/Compose switching trick END
-                                    setContent {
-                                        ComposeOverlay(selectedMode = selectedMode.value, onModeChanged = { selectedMode.value = it }) // <--- This is our Compose components we want to draw over WorldWindGLCanvas(Swing)
-                                    }
-                                }
-                            }
-                        )
+    Column {
+        var leftPanelShown by remember { mutableStateOf(false) }
+        var rightPanelShown by remember { mutableStateOf(false) }
+        var bottomPanelShown by remember { mutableStateOf(false) }
+        var swingComposeShown by remember { mutableStateOf(true) }
+        var composeShown by remember { mutableStateOf(true) }
+        var mapShown by remember { mutableStateOf(true) }
+        var overlaysLeft by remember { mutableStateOf(true) }
+        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+            Button(onClick = { leftPanelShown = !leftPanelShown }) {
+                Text(if (leftPanelShown) "Hide Left" else "Show Left")
+            }
+            Button(onClick = { bottomPanelShown = !bottomPanelShown }) {
+                Text(if (bottomPanelShown) "Hide Bottom" else "Show Bottom")
+            }
+            Button(onClick = { rightPanelShown = !rightPanelShown }) {
+                Text(if (rightPanelShown) "Hide Right" else "Show Right")
+            }
+            Button(onClick = { mapShown = !mapShown }) {
+                Text(if (mapShown) "Hide Map" else "Show Map")
+            }
+            Button(onClick = { swingComposeShown = !swingComposeShown }) {
+                Text(if (swingComposeShown) "Hide Swing Compose" else "Show Swing Compose")
+            }
+            Button(onClick = { composeShown = !composeShown }) {
+                Text(if (composeShown) "Hide Compose" else "Show Compose")
+            }
+            Button(onClick = { overlaysLeft = !overlaysLeft }) {
+                Text(if (overlaysLeft) "Move Right" else "Move Left")
+            }
+        }
+        Row(Modifier.weight(1f)) {
+            if (leftPanelShown) {
+                Box(
+                    Modifier.width(200.dp).fillMaxHeight().background(Color.Blue),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Left Panel")
+                }
+            }
+            val selectedMode = remember { mutableStateOf(Mode.Control) }
+            Box(Modifier.weight(1f)) { // <--- Here we use Box to make layering of SwingPanels
+                if (mapShown) {
+                    // Bottom MAP layer
+                    SwingPanel( // <--- Here we use Swing panel to show WorldWindGLCanvas from Swing world (proper use of SwingPanel)
+                        modifier = Modifier.fillMaxSize().background(Color.Black),
+                        factory = { createJPanelWithWorldWindMap() }
+                    )
+                }
+                Row {
+                    if (!overlaysLeft) {
+                        Spacer(Modifier.weight(1f))
+                    }
+                    OverlaysUI(Modifier.padding(8.dp).width(100.dp), swingComposeShown, composeShown)
+                    if (overlaysLeft) {
+                        Spacer(Modifier.weight(1f))
                     }
                 }
             }
+            if (rightPanelShown) {
+                Box(
+                    Modifier.width(200.dp).fillMaxHeight().background(Color.Magenta),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Right Panel")
+                }
+            }
         }
-    )
-}
-
-@Composable
-private fun ComposeOverlay(selectedMode: Mode, onModeChanged: (Mode) -> Unit) {
-    Box(
-        Modifier.fillMaxHeight(),
-        contentAlignment = Alignment.CenterStart
-    ) {
-        Column(
-            modifier = Modifier.background(
-                shape = RoundedCornerShape(topEnd = 4.dp, bottomEnd = 4.dp),
-                color = Color.DarkGray.copy(alpha = 0.5f)
-            )
-        ) {
-            ModeTab(
-                modifier = Modifier.clickable { onModeChanged(Mode.Control) }
-                    .padding(top = 16.dp, bottom = 8.dp),
-                "Control",
-                selected = selectedMode == Mode.Control
-            )
-            ModeTab(
-                modifier = Modifier.clickable { onModeChanged(Mode.Plan)  }
-                    .padding(top = 8.dp, bottom = 8.dp),
-                "Plan",
-                selected = selectedMode == Mode.Plan
-            )
+        if (bottomPanelShown) {
+            Box(Modifier.fillMaxWidth().height(100.dp).background(Color.Cyan), contentAlignment = Alignment.Center) {
+                Text("Bottom Panel")
+            }
         }
     }
 }
+
+@Composable
+private fun OverlaysUI(modifier: Modifier, swingComposeShown: Boolean, composeShown: Boolean) {
+    Column(modifier, verticalArrangement = Arrangement.spacedBy(4.dp)) {
+        if (swingComposeShown) {
+            SwingPanel( // <--- Here we use Swing panel to make a trick wrapping Compose layout (Swing/Compose switching trick START). If we do not wrap Compose with SwingPanel here, you will not see Compose content over WorldWindGLCanvas(Swing)
+                modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Yellow, RoundedCornerShape(12.dp)),
+                factory = {
+                    ComposePanel().apply { // <-- Swing/Compose switching trick END
+                        setContent {
+                            Box(
+                                Modifier.fillMaxSize().background(Color.Green, RoundedCornerShape(12.dp)),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text("Swing Compose")
+                            }
+                        }
+                    }
+                }
+            )
+        }
+        if (composeShown) {
+            Box(
+                Modifier.fillMaxWidth().weight(1f).background(Color.Green, RoundedCornerShape(12.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Compose")
+            }
+        }
+    }
+}
+
 
 private fun createJPanelWithWorldWindMap(): JPanel {
     return JPanel().apply {
         layout = BoxLayout(this, BoxLayout.Y_AXIS)
         this.background = java.awt.Color.RED
         add(WorldWindowGLCanvas().apply {
+            preferredSize = Dimension(10, 10)
             model = BasicModel()
         })
     }
@@ -132,4 +182,39 @@ fun ModeTab(
         Icon(modifier = Modifier.size(32.dp), imageVector = icon, contentDescription = null, tint = Color.White)
         Text(text = text, color = Color.White, fontSize = 10.sp)
     }
+}
+
+private fun setupSkikoRenderAPI() {
+    when (getOS()) {
+        OS.LINUX, OS.WINDOWS -> System.setProperty("skiko.renderApi", "OPENGL")
+        OS.MAC -> System.setProperty("skiko.renderApi", "METAL")
+        else -> {}
+    }
+}
+
+fun getOS(): OS? {
+    val os = System.getProperty("os.name").lowercase(Locale.getDefault())
+    return when {
+        os.contains("win") -> {
+            OS.WINDOWS
+        }
+
+        os.contains("nix") || os.contains("nux") || os.contains("aix") -> {
+            OS.LINUX
+        }
+
+        os.contains("mac") -> {
+            OS.MAC
+        }
+
+        os.contains("sunos") -> {
+            OS.SOLARIS
+        }
+
+        else -> null
+    }
+}
+
+enum class OS {
+    WINDOWS, LINUX, MAC, SOLARIS, ANDROID
 }
